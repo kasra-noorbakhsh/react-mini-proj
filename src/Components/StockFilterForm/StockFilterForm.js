353b6b5b0f7689be6a83929a74e537ae679df5b1
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-08-01 02:08:07 +0330

    some progress

--- src/Components/StockFilterForm/StockFilterForm.js
+++ src/Components/StockFilterForm/StockFilterForm.js
@@ -1,25 +1,38 @@
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import { schema } from "./stockFilterFormLogic";
+import { useReducer } from "react"
+import { formReducer,  formInitialState, updateStateOnChange, updateStateOnBlur, FieldNames} from "./stockFilterFormLogic"
 
 const StockFilterForm = ({onSubmit}) => {
 
-    const {register, handleSubmit, formState:{errors}} = useForm({resolver: yupResolver(schema)})
+    const [state, dispatch] = useReducer(formReducer, formInitialState)
 
     const submit = () => {
         onSubmit()
     }
 
+    const onChangeProcess = (event) => {
+        updateStateOnChange(state, event.target.name, event.target.value, dispatch)
+    }
+
+    const blockNonNumericOnKeyDown = (event) => {
+        if (!/[0-9]/.test(event.key) && event.key !== 'Backspace') {
+            event.preventDefault();
+        }
+    }
+
+    const onBlurProcess = (event) => {
+        updateStateOnBlur(state, event.target.name, dispatch);
+    }
+
     return(
-        <form onSubmit={handleSubmit(submit)}>
-            <input type="text" placeholder="نماد..." {...register("symbol")}/>
-            {errors.symbol && <p> {errors.symbol.message}</p>}
-            <input type="text" placeholder="نام شرکت..." {...register("company")}/>
-            <input type="number" placeholder="حداکثر قیمت خرید..." {...register("maxPrice")}/>
-            {errors.maxPrice && <p> {errors.maxPrice.message}</p>}
-            <input type="number" placeholder="حداقل قیمت خرید..." {...register("minPrice")}/>
-            {errors.minPrice && <p> {errors.minPrice.message}</p>}
-            <input type="text" placeholder="حوزه صنعتی..." {...register("industry")}/>
+        <form onSubmit={submit}>
+            <input type="text" placeholder="نماد..." name={FieldNames.symbol} onChange={onChangeProcess} onBlur={onBlurProcess}/>
+            {/* {errors.symbol && <p> {errors.symbol.message}</p>} */}
+            <input type="text" placeholder="نام شرکت..." name={FieldNames.companyName} onChange={onChangeProcess} onBlur={onBlurProcess}/>
+            <input type="number" placeholder="حداکثر قیمت خرید..." name={FieldNames.maxPrice} onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
+            {/* {errors.maxPrice && <p> {errors.maxPrice.message}</p>} */}
+            <input type="number" placeholder="حداقل قیمت خرید..." name={FieldNames.minPrice} onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
+            {/* {errors.minPrice && <p> {errors.minPrice.message}</p>} */}
+            <input type="text" placeholder="حوزه صنعتی..." name={FieldNames.industry} onChange={onChangeProcess} onBlur={onBlurProcess}/>
             <input type="submit" value="ثبت"/>
         </form>
     )
--- src/Components/StockFilterForm/stockFilterFormLogic.js
+++ src/Components/StockFilterForm/stockFilterFormLogic.js
@@ -1,27 +1,85 @@
-import * as yup from "yup";
 
-yup.addMethod(yup.number, "compareMinMax", function(errorMassege) {
-    return this.test("compareMinMax", errorMassege, function(val) {
-        const { path, createError } = this;
-        const max = this.resolve(yup.ref("maxPrice"));
-        return (max < 0) || val < max || createError({ path, message: errorMassege || "min max validation failed" });
-    });
-});
+// yup.addMethod(yup.number, "compareMinMax", function(errorMassege) {
+//     return this.test("compareMinMax", errorMassege, function(val) {
+//         const { path, createError } = this;
+//         const max = this.resolve(yup.ref("maxPrice"));
+//         return (max < 0) || val < max || createError({ path, message: errorMassege || "min max validation failed" });
+//     });
+// });
 
-export const schema = yup.object().shape({
-    symbol: yup.string().transform((value, originalValue) => {
-        return (originalValue === "") ? null: value
-    }).min(3, "حداقل طول نماد رعایت نشده است").max(6, "حداکثر طول نماد رعایت نشده است").nullable(true),
+import { object } from "yup";
 
-    company: yup.string(),
+// export const schema = yup.object().shape({
+//     symbol: yup.string().transform((value, originalValue) => {
+//         return (originalValue === "") ? null: value
+//     }).min(3, "حداقل طول نماد رعایت نشده است").max(6, "حداکثر طول نماد رعایت نشده است").nullable(true),
 
-    maxPrice: yup.number().transform((value, originalValue) => {
-        return (originalValue === "") ? Infinity: value
-    }).min(0, "عدد وارد شده منفی است").nullable(true),
+//     company: yup.string(),
 
-    minPrice: yup.number().min(0, "عدد وارد شده منفی است").transform((value, originalValue) => {
-        return (originalValue === "") ? 0: value
-    }).compareMinMax("حداقل قیمت از حداکثر قیمت بیشتر است").nullable(true),
+//     maxPrice: yup.number().transform((value, originalValue) => {
+//         return (originalValue === "") ? Infinity: value
+//     }).min(0, "عدد وارد شده منفی است").nullable(true),
 
-    industry: yup.string()
+//     minPrice: yup.number().min(0, "عدد وارد شده منفی است").transform((value, originalValue) => {
+//         return (originalValue === "") ? 0: value
+//     }).compareMinMax("حداقل قیمت از حداکثر قیمت بیشتر است").nullable(true),
+
+//     industry: yup.string()
+// })
+
+export const FieldNames = Object.freeze({
+    symbol: "symbol",
+    companyName: "companyName",
+    maxPrice: "maxPrice",
+    minPrice: "minPrice",
+    industry: "industry"
 })
+
+export const formInitialState = {
+    [FieldNames.symbol]: {
+        value: "",
+        focusOnce: false,
+        error: {
+            hasAny: false,
+            message: "",
+        }
+    },
+    [FieldNames.companyName]: {
+        value: "",
+    },
+    [FieldNames.maxPrice]: {
+        value: "",
+        focusOnce: false,
+        error: {
+            hasAny: false,
+            message: ""
+        }
+    },
+    [FieldNames.minPrice]: {
+        value: "",
+        focusOnce: false,
+        error: {
+            hasAny: false,
+            message: ""
+        }
+    },
+    [FieldNames.industry]: {
+        value: "",
+    }
+} 
+
+export const formReducer = (state, {newState}) => {
+    console.log(newState);
+    return newState;
+}
+
+export const updateStateOnChange = (currentState, fieldName, newValue, dispatcher) => {
+    const targetField = currentState[fieldName];
+    const changedField = {...targetField, value: newValue};
+    let tempState = {...currentState, [fieldName]: changedField};
+    dispatcher({newState: tempState});
+}
+
+export const updateStateOnBlur = (currentState, fieldName, dispatcher) => {
+    console.log(fieldName);
+}
