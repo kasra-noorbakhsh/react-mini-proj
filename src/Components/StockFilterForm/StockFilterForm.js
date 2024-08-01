ca2eeac6d60e09d704c0bc4eb52e45bc3c488f0f
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-08-01 12:41:57 +0330

    more changes

--- src/Components/StockFilterForm/StockFilterForm.js
+++ src/Components/StockFilterForm/StockFilterForm.js
@@ -1,16 +1,19 @@
 import { useReducer } from "react"
 import { formReducer,  formInitialState, updateStateOnChange, updateStateOnBlur, FieldNamesEnum} from "./stockFilterFormLogic"
 
+import "./StockFilterForm.css"
+
 const StockFilterForm = ({onSubmit}) => {
 
     const [state, dispatch] = useReducer(formReducer, formInitialState)
 
-    const submit = () => {
-        onSubmit()
+    const submit = (event) => {
+        event.preventDefault();
+        onSubmit();
     }
 
     const onChangeProcess = (event) => {
-        updateStateOnChange(state, event.target.name, event.target.value, dispatch)
+        updateStateOnChange(state, event.target.name, event.target.value, dispatch);
     }
 
     const blockNonNumericOnKeyDown = (event) => {
@@ -24,14 +27,29 @@ const StockFilterForm = ({onSubmit}) => {
     }
 
     return(
-        <form onSubmit={submit}>
-            <input type="text" placeholder="نماد..." name={FieldNamesEnum.symbol} onChange={onChangeProcess} onBlur={onBlurProcess}/>
-            {state.symbol.error.hasAny && <p> {state.symbol.error.message}</p>}
-            <input type="text" placeholder="نام شرکت..." name={FieldNamesEnum.companyName} onChange={onChangeProcess} onBlur={onBlurProcess}/>
-            <input type="number" placeholder="حداکثر قیمت خرید..." name={FieldNamesEnum.maxPrice} onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
-            <input type="number" placeholder="حداقل قیمت خرید..." name={FieldNamesEnum.minPrice} onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
-            {state.minPrice.error.hasAny && <p> {state.minPrice.error.message}</p>}
-            <input type="text" placeholder="حوزه صنعتی..." name={FieldNamesEnum.industry} onChange={onChangeProcess} onBlur={onBlurProcess}/>
+        <form onSubmit={submit} className="stock-filter-form">
+            <div>
+                <label>نماد: </label>
+                <input className="stock-filter-form-input" type="text" placeholder="نماد..." name={FieldNamesEnum.symbol} onChange={onChangeProcess} onBlur={onBlurProcess}/>
+                {state.symbol.error.hasAny && <p className="stock-filter-form-error-message"> {state.symbol.error.message}</p>}
+            </div>
+            <div>
+                <label>نام شرکت: </label>
+                <input className="stock-filter-form-input" type="text" placeholder="نام شرکت..." name={FieldNamesEnum.companyName} onChange={onChangeProcess} onBlur={onBlurProcess}/>
+            </div>
+            <div>
+                <label>حوزه صنعتی: </label>
+                <input className="stock-filter-form-input" type="text" placeholder="حوزه صنعتی..." name={FieldNamesEnum.industry} onChange={onChangeProcess} onBlur={onBlurProcess}/>
+            </div>
+            <div>
+                <label>حداکثر قیمت خرید: </label>
+                <input className="stock-filter-form-input" type="text" placeholder="حداکثر قیمت خرید..." name={FieldNamesEnum.maxPrice} onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
+            </div>
+            <div>
+                <label>حداقل قیمت خرید: </label>
+                <input className="stock-filter-form-input" type="text" placeholder="حداقل قیمت خرید..." name={FieldNamesEnum.minPrice} onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
+                {state.minPrice.error.hasAny && <p className="stock-filter-form-error-message"> {state.minPrice.error.message}</p>}
+            </div>
             <input type="submit" value="ثبت"/>
         </form>
     )
--- src/Components/StockFilterForm/stockFilterFormLogic.js
+++ src/Components/StockFilterForm/stockFilterFormLogic.js
@@ -1,39 +1,10 @@
 
-import { object } from "yup";
-
-// yup.addMethod(yup.number, "compareMinMax", function(errorMassege) {
-//     return this.test("compareMinMax", errorMassege, function(val) {
-//         const { path, createError } = this;
-//         const max = this.resolve(yup.ref("maxPrice"));
-//         return (max < 0) || val < max || createError({ path, message: errorMassege || "min max validation failed" });
-//     });
-// });
-
-
-// export const schema = yup.object().shape({
-//     symbol: yup.string().transform((value, originalValue) => {
-//         return (originalValue === "") ? null: value
-//     }).min(3, "حداقل طول نماد رعایت نشده است").max(6, "حداکثر طول نماد رعایت نشده است").nullable(true),
-
-//     company: yup.string(),
-
-//     maxPrice: yup.number().transform((value, originalValue) => {
-//         return (originalValue === "") ? Infinity: value
-//     }).min(0, "عدد وارد شده منفی است").nullable(true),
-
-//     minPrice: yup.number().min(0, "عدد وارد شده منفی است").transform((value, originalValue) => {
-//         return (originalValue === "") ? 0: value
-//     }).compareMinMax("حداقل قیمت از حداکثر قیمت بیشتر است").nullable(true),
-
-//     industry: yup.string()
-// })
-
 export const FieldNamesEnum = Object.freeze({
     symbol: "symbol",
     companyName: "companyName",
+    industry: "industry",
     maxPrice: "maxPrice",
     minPrice: "minPrice",
-    industry: "industry"
 })
 
 export const formInitialState = {
