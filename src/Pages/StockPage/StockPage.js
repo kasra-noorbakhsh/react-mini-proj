46f43e929299f137afe3c97d3e214baebd3478c2
MMDSour <soor.mmd81@gmail.com>
2024-08-02 21:14:17 +0330

    fetching stocks with comments

--- src/Pages/StockPage/StockPage.js
+++ src/Pages/StockPage/StockPage.js
@@ -1,11 +1,28 @@
 import  {useEffect, useState} from 'react'
 import {useParams, Link} from "react-router-dom"
+import {setStockAsync} from "./stockPageLogic";
 
 import "./StockPage.css"
 
 const StockPage = () => {
+    const {stockId} = useParams()
+
+    const [stock, setStock] = useState({})
+
+
+    useEffect(() => {
+        setStockAsync(setStock, stockId)
+    }, []);
+
     return (
-        <div>this is stockPage</div>
+        <div>
+            <p>{stock.id}</p>
+            <p>{stock.companyName}</p>
+            <p>{stock.symbol}</p>
+            <p>{stock.industry}</p>
+            <p>{stock.purchase}</p>
+            <p>{stock.lastDiv}</p>
+        </div>
     )
 }
 
---
+++ src/Pages/StockPage/stockPageLogic.js
@@ -0,0 +1,28 @@
+import {getStocks} from "../../urls";
+
+const fetchStockAsync = async (url) => {
+    const response = await fetch(url , {
+        method: 'get',
+        headers: {"Content-Type": "application/json"}
+        })
+
+    const data = await response.json()
+    console.log(data)
+    return data
+}
+
+export const setStockAsync = async (setStock, Id) => {
+    const url = `${getStocks}/with-comments/${Id}`
+    const data = await fetchStockAsync(url)
+
+    const tempStocks =  {
+            id: data.id,
+            symbol: data.symbol,
+            companyName: data.companyName,
+            industry: data.industry,
+            purchase: data.purchase,
+            lastDiv: data.lastDiv,
+            comments: data.comments,
+    }
+    setStock(tempStocks);
+}
