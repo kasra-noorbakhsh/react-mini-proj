f1525637634bba799962c59b011d30087d77de31
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-08-03 15:34:49 +0330

    better structure in StockCard commponent

--- src/Components/StockCard/StockCard.js
+++ src/Components/StockCard/StockCard.js
@@ -8,6 +8,8 @@ const StockCard = ({ symbol, companyName, industry, id, onClickProcess: viewStoc
                 <p>نماد: {symbol}</p>
                 <p>نام شرکت: {companyName}</p>
                 <p>حوزه صنعت: {industry}</p>
+            </div>
+            <div>
                 <button className='view-stock-page-btn stock-card-btn' onClick={() => viewStockPage(id)}>مشاهده سهام</button>
                 <button className='delete-stock-btn stock-card-btn'>حذف سهام</button>
             </div>
