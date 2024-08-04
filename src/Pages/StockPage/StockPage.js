c53386c243c7dea180c50a7c20b731e39b1f969a
ShahnamFeyzian <shahnamecfman@gmail.com>
2024-08-04 09:38:38 +0330

    add simple popup for comment

--- src/Pages/StockPage/StockPage.js
+++ src/Pages/StockPage/StockPage.js
@@ -2,21 +2,31 @@ import React from 'react';
 import  {useEffect, useState} from 'react'
 import {useParams} from "react-router-dom"
 import {setStockAsync} from "./stockPageLogic";
+import CommentCreatorPopup from '../../Components/CommentCreatorPopup/CommentCreatorPopup';
 
 import "./StockPage.css"
 
 const StockPage = () => {
     const {stockId} = useParams()
+    const [showCommentPopup, setShowCommentPopup] = useState(false);
 
     const [stock, setStock] = useState({})
-
 
     useEffect(() => {
         setStockAsync(setStock, stockId)
     }, []);
 
+    const opentCommentPopup = () => {
+        setShowCommentPopup(true);
+    }
+
+    const closeCommentPopup = () => {
+        setShowCommentPopup(false);
+    }
+
     return (
         <div>
+            <CommentCreatorPopup show={showCommentPopup} closeFunc={closeCommentPopup}/>
             <div className="info-box">
                 <img src="../aks.jpg" alt="Stock Image" className="info-box-image"/>
                 <div className="info-box-content">
@@ -31,7 +41,7 @@ const StockPage = () => {
                     </ul>
                 </div>
             </div>
-            <button className="stock-page-add-comment-btn">اضافه کردن دیدگاه</button>
+            <button className="stock-page-add-comment-btn" onClick={opentCommentPopup}>اضافه کردن دیدگاه</button>
         </div>
     )
 }
