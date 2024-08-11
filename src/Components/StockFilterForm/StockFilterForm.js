1a42f1a51b8a78be1a6749d476321fc7e199a29b
MMDSour <soor.mmd81@gmail.com>
2024-08-11 18:53:58 +0330

    stock page responsiveness handling

--- src/Components/StockFilterForm/StockFilterForm.js
+++ src/Components/StockFilterForm/StockFilterForm.js
@@ -30,41 +30,48 @@ const StockFilterForm = ({ onSubmit }) => {
 
 	return (
 		<form onSubmit={submit} className="stock-filter-form">
-
-			<div style={{ height: "10px" }}>
-				<label>نماد: </label>
-				<input className="stock-filter-form-input" type="text" placeholder="نماد..." name={FieldNamesEnum.symbol}
-					onChange={onChangeProcess} onBlur={onBlurProcess} />
-				{state.symbol.error.hasAny && <p className="stock-filter-form-error-message"> {state.symbol.error.message}</p>}
+			<div className="form-group">
+				<label>نماد:</label>
+				<input className="stock-filter-form-input" type="text" placeholder="نماد..."
+					   name={FieldNamesEnum.symbol}
+					   onChange={onChangeProcess} onBlur={onBlurProcess}/>
+				{state.symbol.error.hasAny &&
+					<p className="stock-filter-form-error-message">{state.symbol.error.message}</p>}
 			</div>
 
-			<div>
-				<label>نام شرکت: </label>
-				<input className="stock-filter-form-input" type="text" placeholder="نام شرکت..." name={FieldNamesEnum.companyName}
-					onChange={onChangeProcess} onBlur={onBlurProcess} />
+			<div className="form-group">
+				<label>نام شرکت:</label>
+				<input className="stock-filter-form-input" type="text" placeholder="نام شرکت..."
+					   name={FieldNamesEnum.companyName}
+					   onChange={onChangeProcess} onBlur={onBlurProcess}/>
 			</div>
 
-			<div>
-				<label>حوزه صنعتی: </label>
-				<input className="stock-filter-form-input" type="text" placeholder="حوزه صنعتی..." name={FieldNamesEnum.industry}
-					onChange={onChangeProcess} onBlur={onBlurProcess} />
+			<div className="form-group">
+				<label>حوزه صنعتی:</label>
+				<input className="stock-filter-form-input" type="text" placeholder="حوزه صنعتی..."
+					   name={FieldNamesEnum.industry}
+					   onChange={onChangeProcess} onBlur={onBlurProcess}/>
 			</div>
 
-			<div>
-				<label>حداکثر قیمت خرید: </label>
-				<input className="stock-filter-form-input" type="text" placeholder="حداکثر قیمت خرید..." name={FieldNamesEnum.maxPrice}
-					onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown} />
+			<div className="form-group">
+				<label>حداکثر قیمت خرید:</label>
+				<input className="stock-filter-form-input" type="text" placeholder="حداکثر قیمت خرید..."
+					   name={FieldNamesEnum.maxPrice}
+					   onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
 			</div>
 
-			<div>
-				<label>حداقل قیمت خرید: </label>
-				<input className="stock-filter-form-input" type="text" placeholder="حداقل قیمت خرید..." name={FieldNamesEnum.minPrice}
-					onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown} />
-				{state.minPrice.error.hasAny && <p className="stock-filter-form-error-message"> {state.minPrice.error.message}</p>}
+			<div className="form-group">
+				<label>حداقل قیمت خرید:</label>
+				<input className="stock-filter-form-input" type="text" placeholder="حداقل قیمت خرید..."
+					   name={FieldNamesEnum.minPrice}
+					   onChange={onChangeProcess} onBlur={onBlurProcess} onKeyDown={blockNonNumericOnKeyDown}/>
+				{state.minPrice.error.hasAny &&
+					<p className="stock-filter-form-error-message">{state.minPrice.error.message}</p>}
 			</div>
 
-			<input className="stock-filter-form-submit" type="submit" value="ثبت" disabled={!canSubmit(state)} />
+			<input className="stock-filter-form-submit" type="submit" value="ثبت" disabled={!canSubmit(state)}/>
 		</form>
+
 	)
 }
 
